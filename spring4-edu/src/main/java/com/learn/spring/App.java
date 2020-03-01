14fbfa810479fb3cf37bf88be887ec7715d76811
csustzw127 <593136267@qq.com>
2020-03-01 12:53:59 +0800

    web-starter,热部署,profile多环境部署

--- spring4-edu/src/main/java/com/learn/spring/App.java
+++ spring4-edu/src/main/java/com/learn/spring/App.java
@@ -1,18 +1,27 @@
 package com.learn.spring;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 /**
  * Hello world!
  *
  */
+@SpringBootApplication
 public class App 
 {
+    // 此示例主要演示以下内容：
+    // 1.Hello World
+    // 2.配置文件(application.properties)的使用 及 多环境配置文件的设置
+    // 3.基本的MVC Controller单元测试 及 针对某个Class的单元测试
+    // 4.Spring Boot的热部署(修改代码后只需Build一下无须重新Run)
     public static void main( String[] args )
     {
-        AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(MyConfig.class);
-        System.out.println(ctx.getBean(MyBean.class));
-        System.out.println(ctx.getBean("myBean"));
-        ctx.close();
+//        AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(MyConfig.class);
+//        System.out.println(ctx.getBean(MyBean.class));
+//        System.out.println(ctx.getBean("myBean"));
+//        ctx.close();
+        SpringApplication.run(App.class, args);
     }
 }
--- spring4-edu/src/main/java/com/learn/spring/MyConfig.java
+++ spring4-edu/src/main/java/com/learn/spring/MyConfig.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 
-@Configuration
+//@Configuration
 public class MyConfig {
 
     @Bean(name="myBean")
